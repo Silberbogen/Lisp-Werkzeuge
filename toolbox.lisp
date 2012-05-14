72826faf765f9f59e5a96134efe6c840ce51b4b2
Sascha Biermanns <skkd.h4k1n9@yahoo.de>
2012-05-14 10:16:10 +0200

    Austausch cat gegen concatenate

--- toolbox.lisp
+++ toolbox.lisp
@@ -358,6 +358,21 @@ Beispiel: (zwölfter 'a b c d e f g h i j k l m n)) => L"
 
 ; ---------------------
 
+(defmacro ppmx (form)
+  "Pretty prints the macro expansion of FORM."
+  `(let* ((exp1 (macroexpand-1 ',form))
+	  (exp (macroexpand exp1))
+	  (*print-circle* nil))
+     (cond ((equal exp exp1)
+	    (format t "~&Macro expansion:")
+	    (pprint exp))
+	   (t (format t "~&First step of expansion:")
+	      (pprint exp1)
+	      (format t "~%~%Final expansion:")
+	      (pprint exp)))
+     (format t "~%~%")
+     (values)))
+
 ;;; =====================================================================
 
 ;;; -----------------------
