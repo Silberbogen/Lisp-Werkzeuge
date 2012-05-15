8db276ae77588c04f965208f7ec8d63d58602c17
Sascha Biermanns <skkd.h4k1n9@yahoo.de>
2012-05-15 04:15:38 +0200

    Unterstützung für Themen wurde hinzugefügt.

---
+++ lubuntu-thema.lisp
@@ -0,0 +1,41 @@
+;-*-lisp-*-
+;-*- coding: utf-8 -*-
+;;;; ============================================================================================
+;;;; Lubuntu Thema zum Nachladen durch die StumpWM-RC-Datei
+;;;; Version 0.01
+;;;; ============================================================================================
+(in-package :stumpwm)
+
+(setq *browser* (concatenate 'string "chromium-browser" " "))
+
+(setq *dateimanager* (concatenate 'string "pcmanfm" " "))
+
+(setq *email-client* (concatenate 'string "sylpheed" " "))
+
+(setq *software-center* (concatenate 'string "lubuntu-software-center" " "))
+
+(setq *systerm* "exec uxterm -fg CornflowerBlue -bg DarkSlateGrey -class 'Admin-uxterm' -e 'sudo -i'")
+
+(setq *uxthema* "exec uxterm -fg DodgerBlue -bg NavyBlue")
+
+(setq *uxt* "uxterm -fg DodgerBlue -bg NavyBlue -class 'Userterminal'")
+
+(setq *uxt-neu* "uxterm -fg DodgerBlue -bg NavyBlue")
+
+;; Lubuntu 12.04 Thema
+(set-focus-color      "DodgerBlue")
+(set-unfocus-color    "black")
+(set-win-bg-color     "CornflowerBlue")
+(set-fg-color         "NavyBlue")
+(set-bg-color         "PowderBlue")
+
+;; Vorbereitungen für das Modeline-Verhalten
+(setf *mode-line-background-color*      "DodgerBlue3"
+      *mode-line-foreground-color*      "PowderBlue"
+      *mode-line-border-color*          "CornflowerBlue"
+      *mode-line-timeout*               1
+      *mode-line-position*              :bottom
+      *time-format-string-default*      "%a %d.%m.%Y %I:%M%p"
+      *time-modeline-string*            "%a %d.%m.%Y %I:%M%p"
+      *group-format*                    "%n%s%t"
+      *window-format*                   "«%n%s%m%10c»")
