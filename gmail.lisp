887b3d7f899d1dc9b90cd9fc18044e2c8f9a4569
Sascha Biermanns <skkd.h4k1n9@yahoo.de>
2012-08-16 00:53:38 +0200

    .stumpwmrc an Crunchbang Linux angepasst

--- gmail.lisp
+++ gmail.lisp
@@ -10,7 +10,7 @@
 (defvar *gmail-password* "YOURPASSWORD"
   "Password for gmail")
 
-(load "kennwörter.lisp") ; here I saved my *gmail-username* and *gmail-password* separately
+(load "~/Dropbox/Lisp/kennwörter.lisp") ; here I saved my *gmail-username* and *gmail-password* separately
 
 (defparameter *gmail-show-titles* nil
   "When non-NIL, show the authors and titles whenever new mail arrives.")
@@ -29,29 +29,29 @@
        second is a list of (AUTHOR . TITLE) cons cells."
   (when (and *gmail-username* *gmail-password*)
     (multiple-value-bind (response-body response-code)
-	(drakma:http-request "https://mail.google.com/mail/feed/atom" :cookie-jar *gmail-cookies*
-			     :basic-authorization (list *gmail-username* *gmail-password*))
+    (drakma:http-request "https://mail.google.com/mail/feed/atom" :cookie-jar *gmail-cookies*
+                 :basic-authorization (list *gmail-username* *gmail-password*))
       (if (= 401 response-code)
-	  :401-unauthorized
-	  (let* ((feed-tree (xmls:parse response-body))
-		 (fullcount-tag (find "fullcount" (xmls:node-children feed-tree)
-				      :key 'xmls:node-name :test 'equal)))
-	    (assert (string= "feed" (xmls:node-name feed-tree)))
-	    (when (and fullcount-tag
-		       (stringp (first (xmls:node-children fullcount-tag))))
-	      (values (or (read-from-string (first (xmls:node-children fullcount-tag)))
-			  0)
-		      (loop for child in (xmls:node-children feed-tree)
-			 for title-tag = (when (equal (xmls:node-name child) "entry")
-					   (find "title" (xmls:node-children child)
-						 :key 'xmls:node-name :test 'equal))
-			 for author-tag = (when (equal (xmls:node-name child) "entry")
-					    (find "author" (xmls:node-children child)
-						  :key 'xmls:node-name :test 'equal))
-			 when (and title-tag author-tag)
-			 collect (cons
-				  (first (xmls:node-children (first (xmls:node-children author-tag))))
-				  (first (xmls:node-children title-tag)))))))))))
+      :401-unauthorized
+      (let* ((feed-tree (xmls:parse response-body))
+         (fullcount-tag (find "fullcount" (xmls:node-children feed-tree)
+                      :key 'xmls:node-name :test 'equal)))
+        (assert (string= "feed" (xmls:node-name feed-tree)))
+        (when (and fullcount-tag
+               (stringp (first (xmls:node-children fullcount-tag))))
+          (values (or (read-from-string (first (xmls:node-children fullcount-tag)))
+              0)
+              (loop for child in (xmls:node-children feed-tree)
+             for title-tag = (when (equal (xmls:node-name child) "entry")
+                       (find "title" (xmls:node-children child)
+                         :key 'xmls:node-name :test 'equal))
+             for author-tag = (when (equal (xmls:node-name child) "entry")
+                        (find "author" (xmls:node-children child)
+                          :key 'xmls:node-name :test 'equal))
+             when (and title-tag author-tag)
+             collect (cons
+                  (first (xmls:node-children (first (xmls:node-children author-tag))))
+                  (first (xmls:node-children title-tag)))))))))))
 
 
 (defun format-gmail (stream)
@@ -59,26 +59,26 @@
           values if it is called more frequently than once every *GMAIL-PING-PERIOD*.  When new mail
           arrives, this function will also display a message containing all the current inbox items"
   (when (> (- (get-internal-real-time)
-	      *gmail-last-ping*)
-	   *gmail-ping-period*)
+          *gmail-last-ping*)
+       *gmail-ping-period*)
     (multiple-value-bind (num-msgs msg-summaries)
-	(ping-gmail)
+    (ping-gmail)
       (when (and *gmail-show-titles*
-		 num-msgs (> num-msgs 0)
-		 (or (null *gmail-last-value*)
-		     (/= num-msgs *gmail-last-value*)))
-	(let ((*timeout-wait* (* *timeout-wait* (min 2 num-msgs)))) ; leave time to read the titles
-	  (message "~A"
-		   (with-output-to-string (s)
-		     (loop for (author . title) in msg-summaries
-			do (format s "~A - ~A~%" author title))))))
+         num-msgs (> num-msgs 0)
+         (or (null *gmail-last-value*)
+             (/= num-msgs *gmail-last-value*)))
+    (let ((*timeout-wait* (* *timeout-wait* (min 2 num-msgs)))) ; leave time to read the titles
+      (message "~A"
+           (with-output-to-string (s)
+             (loop for (author . title) in msg-summaries
+            do (format s "~A - ~A~%" author title))))))
       (setf *gmail-last-value* num-msgs
-	    *gmail-last-ping* (get-internal-real-time))))
+        *gmail-last-ping* (get-internal-real-time))))
   (cond
     ((null *gmail-last-value*)
      (format stream "[^Bmail:???^b]"))
     ((and (numberp *gmail-last-value*)
-	  (zerop *gmail-last-value*))
+      (zerop *gmail-last-value*))
      (format stream "[^Bmail:^b 0]"))
     ((numberp *gmail-last-value*)
      (format stream "[^BMAIL:~2D^b]" *gmail-last-value*))
