36eb2f22ca9743137e56301f1ef1f123061fed79
Sascha Biermanns <skkd.h4k1n9@yahoo.de>
2014-09-24 23:57:53 +0200

    Lösung zu Aufgabe 40 hinzugefügt

--- euler.lisp
+++ euler.lisp
@@ -1117,8 +1117,21 @@ Answer:	840"
 		(when (> anzahl max-anzahl)
 		  (setf max-anzahl anzahl
 				max-p p))))))
-		
-  
+
+
+
+(defun euler-40 ()
+  "Champernowne's constant
+Problem 40
+An irrational decimal fraction is created by concatenating the positive integers:
+0.123456789101112131415161718192021...
+It can be seen that the 12th digit of the fractional part is 1.
+If dn represents the nth digit of the fractional part, find the value of the following expression.
+d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+Answer:	210"
+  (let ((digits (loop for i from 0 to 200000 append (zahl->ziffern i))))
+	(princ digits)
+	(apply #'* (mapcar #'(lambda (n) (nth n digits)) '(1 10 100 1000 10000 100000 1000000)))))
   
 
 (defun euler-67 ()
