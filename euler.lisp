24c0f710d8616f53b2a85175844c6e64afdded4a
Sascha Biermanns <skkd.h4k1n9@yahoo.de>
2014-09-26 19:28:30 +0200

    Lösung zu Aufgabe 57 hinzugefügt

--- euler.lisp
+++ euler.lisp
@@ -1583,9 +1583,41 @@ Answer:	972"
 		(let ((wert (ziffer-summe (expt a b))))
 		  (when (> wert maximum)
 			(setf maximum wert)))))))
-  
-  
 
+
+
+(defun euler-57 ()
+  "Square root convergents
+Problem 57
+It is possible to show that the square root of two can be expressed as an infinite continued fraction.
+√ 2 = 1 + 1/(2 + 1/(2 + 1/(2 + ... ))) = 1.414213...
+By expanding this for the first four iterations, we get:
+1 + 1/2 = 3/2 = 1.5
+1 + 1/(2 + 1/2) = 7/5 = 1.4
+1 + 1/(2 + 1/(2 + 1/2)) = 17/12 = 1.41666...
+1 + 1/(2 + 1/(2 + 1/(2 + 1/2))) = 41/29 = 1.41379...
+The next three expansions are 99/70, 239/169, and 577/408, but the eighth expansion, 1393/985, is the first example where the number of digits in the numerator exceeds the number of digits in the denominator.
+In the first one-thousand expansions, how many fractions contain a numerator with more digits than denominator?
+Answer:	153"
+  (labels ((nächste-expansion (frac)
+			 (+ 1 (/ 1 (+ 1 frac))))
+		   (expansions-schleife (durchläufe)
+			 (loop for i from 0 to durchläufe
+				as bruch = 1 then (nächste-expansion bruch)
+				collecting bruch))
+		   (zähle-treffer (brüche)
+			 (let ((anzahl 0))
+			   (dolist (bruch brüche anzahl)
+				 (when (> (length (zahl->liste (numerator bruch)))
+						  (length (zahl->liste (denominator bruch))))
+				   (incf anzahl))))))
+	(zähle-treffer (expansions-schleife 1000))))
+
+
+
+
+
+  
 (defun euler-67 ()
   "Maximum path sum II
 Problem 67
