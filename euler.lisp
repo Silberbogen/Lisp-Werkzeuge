f31c32af8883aac3107df585ebbd18679306553a
Sascha Biermanns <skkd.h4k1n9@yahoo.de>
2014-09-26 14:18:10 +0200

    Lösung zu Aufgabe 53 hinzugefügt

--- euler.lisp
+++ euler.lisp
@@ -1462,6 +1462,34 @@ Answer:	142857"
 				   (return i)))))
 	(finde-gleiche-ziffern 6)))
 
+
+
+(defun euler-53 (&optional (limit 100) (minimum 1000000))
+  "Combinatoric selections
+Problem 53
+There are exactly ten ways of selecting three from five, 12345:
+123, 124, 125, 134, 135, 145, 234, 235, 245, and 345
+In combinatorics, we use the notation, 5C3 = 10.
+In general,
+nCr = 	
+n!
+r!(n−r)!
+	,where r ≤ n, n! = n×(n−1)×...×3×2×1, and 0! = 1.
+It is not until n = 23, that a value exceeds one-million: 23C10 = 1144066.
+How many, not necessarily distinct, values of  nCr, for 1 ≤ n ≤ 100, are greater than one-million?
+Answer:	4075"
+  (labels ((möglichkeit (n r)
+			 (/ (faktor n) (faktor r) (faktor (- n r)))))
+	(let ((summe 0))
+	  (do ((i 1 (1+ i)))
+		  ((> i limit)
+		   summe)
+		(dotimes (j i)
+		  (when (> (möglichkeit i j) minimum)
+			(incf summe)))))))
+  
+  
+
 (defun euler-67 ()
   "Maximum path sum II
 Problem 67
